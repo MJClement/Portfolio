42c8d343ee3a893e058a220474176537264a8bc7
MJClement <mitchellclement@yahoo.co.nz>
2020-08-02 19:44:13 +1200

    DEL Load

--- js/portfolio.js
+++ js/portfolio.js
@@ -49,18 +49,18 @@ function slidelink(){
 }
 
 
-function init() {
-  const [loader] = document.querySelectorAll('.loadingpage');
-
-  function timeout(ms) {
-    return () => new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  Promise.resolve()
-    .then(timeout(2000))  // time until fade out animation begins
-    .then(() => loader.classList.add('fadeOut'))
-    .then(timeout(3000))  // time until loader removed from DOM allowing app interactiviy
-    .then(() => loader.style.display = 'none')
-}
-
-init();
+// function init() {
+//   const [loader] = document.querySelectorAll('.loadingpage');
+//
+//   function timeout(ms) {
+//     return () => new Promise(resolve => setTimeout(resolve, ms))
+//   }
+//
+//   Promise.resolve()
+//     .then(timeout(2000))  // time until fade out animation begins
+//     .then(() => loader.classList.add('fadeOut'))
+//     .then(timeout(3000))  // time until loader removed from DOM allowing app interactiviy
+//     .then(() => loader.style.display = 'none')
+// }
+//
+// init();

7a04d52972aefff932b165f2f3cfd376248e2366
MJClement <mitchellclement@yahoo.co.nz>
2020-08-02 19:27:24 +1200

    Practice Loading Page

--- js/portfolio.js
+++ js/portfolio.js
@@ -47,3 +47,20 @@ function slidelink(){
  else if (whichimage == 2)
   window.location = "link3.htm"
 }
+
+
+function init() {
+  const [loader] = document.querySelectorAll('.loadingpage');
+
+  function timeout(ms) {
+    return () => new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  Promise.resolve()
+    .then(timeout(2000))  // time until fade out animation begins
+    .then(() => loader.classList.add('fadeOut'))
+    .then(timeout(3000))  // time until loader removed from DOM allowing app interactiviy
+    .then(() => loader.style.display = 'none')
+}
+
+init();
